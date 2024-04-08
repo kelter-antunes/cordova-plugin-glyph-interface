f7d748510f5edcfef8738cc08a0ee5ef12ea843d
Miguel Antunes <miguel.aka.kelter@gmail.com>
2024-04-08 17:54:34 +0100

    Update GlyphInterface.js

--- www/GlyphInterface.js
+++ www/GlyphInterface.js
@@ -2,39 +2,30 @@ var exec = require('cordova/exec');
 
 var GlyphInterface = {
     builder: function(options, success, error) {
-        // Adjusted to accept an options object
         exec(success, error, 'GlyphInterfacePlugin', 'builder', [options]);
     },
     getPlatform: function(success, error) {
         exec(success, error, 'GlyphInterfacePlugin', 'getPlatform', []);
     },
     channel: function(options, success, error) {
-        // Adjusted to accept an options object
         exec(success, error, 'GlyphInterfacePlugin', 'channel', [options]);
     },
-    // The following methods seem to require adjustments for accepting parameters
     build: function(options, success, error) {
-        // Adjusted to accept an options object
         exec(success, error, 'GlyphInterfacePlugin', 'build', [options]);
     },
     toggle: function(options, success, error) {
-        // Adjusted to accept an options object
         exec(success, error, 'GlyphInterfacePlugin', 'toggle', [options]);
     },
     setPeriod: function(options, success, error) {
-        // Adjusted to accept an options object
         exec(success, error, 'GlyphInterfacePlugin', 'setPeriod', [options]);
     },
     setCycles: function(options, success, error) {
-        // Adjusted to accept an options object
         exec(success, error, 'GlyphInterfacePlugin', 'setCycles', [options]);
     },
     setInterval: function(options, success, error) {
-        // Adjusted to accept an options object
         exec(success, error, 'GlyphInterfacePlugin', 'setInterval', [options]);
     },
     animate: function(options, success, error) {
-        // Adjusted to accept an options object
         exec(success, error, 'GlyphInterfacePlugin', 'animate', [options]);
     },
     getPlatformVersion: function(success, error) {
