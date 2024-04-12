73853bf09d956e64b0b6df83f4b6721775a1c8e9
Miguel Antunes <miguel.aka.kelter@gmail.com>
2024-04-12 15:22:38 +0100

    Update GlyphInterface.js

--- www/GlyphInterface.js
+++ www/GlyphInterface.js
@@ -69,8 +69,8 @@ const GlyphInterface = {
         this.execPlugin('addFrameToBuilder', [builderId, channels], success, error);
     },
     addFrameAnimatedToBuilder: function (builderId, options, success, error) {
-        this.execPlugin('addFrameAnimatedToBuilder', [builderId, JSON.stringify(options)], success, error);
-    },
+        this.execPlugin('addFrameAnimatedToBuilder', [builderId, options], success, error);
+    },    
     clearBuilderFrames: function (success, error) {
         this.execPlugin('clearBuilderFrames', [], success, error);
     },
